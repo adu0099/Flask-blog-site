b1132697875bf14499efceb9dfef46d974b9c20a
Adu <ismailadnan888gmail.com>
2026-02-28 14:48:41 +0600

    Initial commit

---
+++ flaskblog/config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+
+class config:
+    
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME =  os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD =  os.environ.get('EMAIL_PASS')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
